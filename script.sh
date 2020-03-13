62a90e920e7e77e122f7cb24c00c399fc4ed272a
Akram-abdl <60041304+Akram-abdl@users.noreply.github.com>
2020-03-13 09:27:44 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -18,33 +18,29 @@ fi
 
 # create lock file
 touch /tmp/out/lock || { 
-    error_createl = "script failed, cannot create lock file. Error 22 for $file"
+    error_createl = "script failed, cannot create lock file. Error for $file"
     echo $error_createl >> /tmp/out/logs.txt
-    echo $error_createl
-    exit 22 
+    echo $error_createl 
 }
 
 # loops through the files in /tmp/in/, compresses them and moves them to /tmp/out/
 for file in /tmp/in/*
 do 
     gzip  "$file" && echo "success compressing file $file" >> /tmp/out/logs.txt || {
-        error_comp="script failed cannot compress files. Error 22 for $file"
+        error_comp="script failed cannot compress files. Error for $file"
         echo $error_comp >> /tmp/out/logs.txt 
         echo $error_comp
-        exit 22
     }
     mv "$file".gz /tmp/out && echo "success moving file $file to folder" >> /tmp/out/logs.txt || { 
-        error_move = "script failed, cannot move files to /tmp/out. Error 22 for $file"
+        error_move = "script failed, cannot move files to /tmp/out. Error for $file"
         echo $error_move >> /tmp/out/logs.txt
         echo $error_move
-        exit 22
     }
 done
 
 # deletes lock which means that we can run the program again if needed
 rm /tmp/out/lock || { 
-    error_deletel = "script failed, cannot delete lock file. Error 22 for $file"
+    error_deletel = "script failed, cannot delete lock file. Error for $file"
     echo $error_deletel >> /tmp/out/logs.txt 
     echo $error_deletel
-    exit 22
 }
